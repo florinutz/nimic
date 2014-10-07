e7b357691c562daf0b84a568fa1981cee0b009dc
florin <florinutz@gmail.cm>
2014-10-07 23:59:46 +0300

    shebang

--- nimic.php
+++ nimic.php
@@ -1,11 +1,8 @@
+#!/usr/bin/php
 <?php
 // florin, 10/6/14, 10:26 PM
 use Flo\Kernel\Kernel;
-
 $loader = require 'vendor/autoload.php';
-
 $kernel = new Kernel(true);
-
 $app = $kernel->getContainer()->get('app');
-
 $app->run();
